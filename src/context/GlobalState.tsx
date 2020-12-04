eff8066e9d8d4fbf85203dc69999f77a3111059a
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-04 21:13:11 +0500

    created Global state

---
+++ src/context/GlobalState.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, useReducer, useContext } from 'react';
+import AppReducer from './AppReducer';
+
+// type Transaction = {
+//   id: number;
+//   text: string;
+//   amount: number;
+// };
+
+// type GlobalContextType = {
+//   transactions: any[];
+// };
+
+const dummyTransactions = [
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 },
+];
+
+// create initialState
+const initialState: any = {
+  transactions: dummyTransactions,
+};
+
+// create context
+export const GlobalContext = createContext(initialState);
+
+// create Global Provider
+export const GlobalProvider: React.FC<any> = ({ children }: any) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={[state, dispatch]}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+// create store
+export const useStore = () => useContext(GlobalContext);
