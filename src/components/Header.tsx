be94d083f4e67b05db99106505fffcad673e7f92
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-04 21:13:55 +0500

    created Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <h2>Expense Tracker</h2>
+      <h5>By Ahmed Ali</h5>
+      <p className="react-js">(React Typescript PWA)</p>
+    </>
+  );
+};
+
+export default Header;
