fecaca5063677759691a725bc933df8d11aa4937
ahmedali8 <imahmedalibhatti@gmail.com>
2020-12-05 18:57:55 +0500

    added report web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { GlobalProvider } from './context/GlobalState';
+import reportWebVitals from './reportWebVitals';
+import swDev from './swDev';
 
 ReactDOM.render(
   <GlobalProvider>
@@ -9,3 +11,9 @@ ReactDOM.render(
   </GlobalProvider>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+swDev();
